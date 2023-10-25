62c90732b0799926fa510830a64f0ecc1b2d784f
Jesse Bloom <jesse.bloom@ufl.edu>
2023-10-25 12:33:51 -0400

    Fix this

--- version_control.py
+++ version_control.py
@@ -12,6 +12,24 @@ def encode(in_password):
         in_password = str(int(in_password) // 10)
     return out_password
 
+#Decode created by Jesse Bloom
+def decode(out_password):
+    decoded_password = ""
+    for num in out_password:
+        if num == "0":
+            decoded_password += "7"
+        elif num == "1":
+            decoded_password += "8"
+        elif num == "2":
+            decoded_password += "9"
+        else:
+            num=int(num)
+            num-=3
+            decoded_password +=str(num)
+    return decoded_password
+
+
+
 if __name__ == "__main__":
     option = 0
     password = ""
@@ -22,10 +40,11 @@ if __name__ == "__main__":
         option = int(input("Please enter an option: "))
         if option ==1:
             in_pass = input("Please enter your password to encode: ")
-            password = encode(in_pass)
+            out_password = encode(in_pass)
             print("Your password has been encoded and stored!\n")
         elif option ==2:
-            print("The encoded password is " + password+", and the original password is " + decode(password)+".\n")
+            decoded_password=decode(out_password)
+            print("The encoded password is " + out_password+", and the original password is " + decoded_password+".\n")
         elif option ==3:
             break
         elif option !=3:
